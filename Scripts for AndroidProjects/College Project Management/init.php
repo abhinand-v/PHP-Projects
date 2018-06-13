7967f29b51c48baf5e383ddb37b11b386401503b
Abhinand Valasseri <40170836+abhinand-v@users.noreply.github.com>
2018-06-13 22:57:01 +0530

    Update init.php

--- Scripts for AndroidProjects/College Project Management/init.php
+++ Scripts for AndroidProjects/College Project Management/init.php
@@ -1,7 +1,7 @@
 <?php
-$username = 'id1327671_project_admin';
-$password = 'nopassword';
-$database = 'id1327671_macfast_proj_man';
+$username = 'project_admin';
+$password = 'password';
+$database = 'macfast_proj_man';
 $server = 'localhost';
 $con = mysqli_connect($server,$username,$password,$database);
 if(!$con)
